b67589f18006c11793da82c63a0b63d6fa06e747
JAESEON PARK <ddorri83@naver.com>
2026-02-25 01:46:29 +0900

    chore: 로고이미지 없을경우 기본 비즈니스빌딩이미지 추가

--- src/components/workspace/WorkspaceCard.tsx
+++ src/components/workspace/WorkspaceCard.tsx
@@ -5,6 +5,7 @@ import {
   type TMenuItem,
 } from "@/components/common/dropdownmenu/DropdownMenu";
 
+import BuildingIcon from "@/assets/icon/workspace/building.svg?react";
 import VectorIcon from "@/assets/icon/workspace/Vector.svg?react";
 
 type TProps = {
@@ -16,15 +17,19 @@ export default function WorkspaceCard({ workspace: w, menuItems }: TProps) {
   return (
     <li className="flex items-center justify-between rounded-component-md bg-white px-6 py-5 shadow-sm border border-gray-100">
       <div className="flex items-center gap-5 min-w-0">
-        <div className="w-20 h-20 bg-gray-200 shrink-0 rounded-component-sm">
-          {/* TODO: 스타일 확인을 위해 bg-gray-200넣어둠. API연동할때 삭제예정 */}
+        <div className="w-20 h-20 bg-gray-100 shrink-0 rounded-component-sm">
+          {/* TODO: 스타일 확인을 위해 bg-gray-100넣어둠. API연동할때 삭제예정 */}
           {w.logoUrl ? (
             <img
               src={w.logoUrl}
               alt={`${w.name} 로고`}
-              className="w-full h-full object-cover rounded-b-component-sm"
+              className="w-full h-full object-cover rounded-component-sm"
             />
-          ) : null}
+          ) : (
+            <div className="w-full h-full flex items-center justify-center">
+              <BuildingIcon className="w-12 h-12" />
+            </div>
+          )}
         </div>
 
         <div className="min-w-0">
