eee74602ebbfa4bc72fad567cc0aa9b9959c42c0
YermIm <iyr7773@naver.com>
2026-03-02 02:28:18 +0900

    fix: ControlBox 컴포넌트 기본값 수정

--- src/components/common/controlbox/ControlBox.stories.tsx
+++ src/components/common/controlbox/ControlBox.stories.tsx
@@ -13,10 +13,9 @@ type TControlBoxStory = StoryObj<typeof ControlBox>;
 
 export const Default: TControlBoxStory = {
   args: {
-    title: "캠페인 통합 운영 제어",
-    description:
-      "여러 광고 플랫폼의 캠페인을 하나로 묶어 성과와 운영 상태를 통합 관리합니다.\n광고 플랫폼 로그인 후 캠페인을 불러와 연결합니다.",
-    buttonText: "캠페인 통합 연동하기",
+    title: "제목",
+    description: "설명",
+    buttonText: "버튼",
     onButtonClick: () => console.log("click"),
     className: "w-full",
   },
