c37d4aa1385384422f90a75eed9a0f59ce6f4234
서제경 <seojk0315@naver.com>
2026-03-16 16:47:44 +0900

    refactor: modalcontent와 닫기 버튼 사이 간격 추가

--- src/components/common/modal/ModalContent.tsx
+++ src/components/common/modal/ModalContent.tsx
@@ -22,7 +22,7 @@ export default function ModalContent({
   variant = "danger",
 }: IModalContentProps) {
   return (
-    <div className="text-center px-2 py-6">
+    <div className="text-center px-2 pt-10 pb-6">
       <div className="flex justify-center items-center gap-2 mb-3">
         {icon && <div className="shrink-0">{icon}</div>}
         <h3 className="font-heading2 text-text-main leading-none">{title}</h3>
