ecf62f9bca04449b5aa8a1dc25f5fcc3ca7a58d2
YermIm <iyr7773@naver.com>
2026-03-31 03:20:52 +0900

    refactor: 로직 수정

--- src/layout/main/MainLayout.tsx
+++ src/layout/main/MainLayout.tsx
@@ -13,32 +13,39 @@ export default function MainLayout() {
   useCoreQuery(["myInfo"], getMyInfo);
 
   const setSelectedOrgId = useWorkspaceStore((s) => s.setSelectedOrgId);
-  const selectedOrgId = useWorkspaceStore((s) => s.selectedOrgId);
 
-  const { data: workspaces } = useCoreQuery(["my-workspaces"], getMyWorkspaces);
-  const { data: savedData } = useCoreQuery(
+  const savedWorkspaceQuery = useCoreQuery(
     ["savedWorkspace"],
     getSavedWorkspace,
   );
+  const { data: savedData } = savedWorkspaceQuery;
+  const { data: workspaces } = useCoreQuery(["my-workspaces"], getMyWorkspaces);
+  const selectedOrgId = useWorkspaceStore((s) => s.selectedOrgId);
 
   useEffect(() => {
-    if (!workspaces || workspaces.length === 0) return;
+    // 모두 완료되었을 때 시작
+    if (!workspaces?.length || !savedWorkspaceQuery.isFetched) return;
+    if (selectedOrgId !== null) return;
 
     const savedId = savedData?.orgId;
     const isExist = workspaces.some((w) => w.orgId === savedId);
 
-    if (selectedOrgId) return;
-    if (!workspaces || workspaces.length === 0) return;
     // 1. 현재 워크스페이스 조회 API 데이터
     if (savedId !== undefined && isExist) {
       setSelectedOrgId(savedId);
     }
-    // 2. isCurrentWorkspace
+    // 2. isCurrentWorkspace 또는 첫 번째
     else {
       const currentOrg = workspaces.find((w) => w.isCurrentWorkspace);
       setSelectedOrgId(currentOrg?.orgId || workspaces[0].orgId);
     }
-  }, [workspaces, savedData, setSelectedOrgId, selectedOrgId]);
+  }, [
+    workspaces,
+    savedWorkspaceQuery.isFetched,
+    savedData,
+    setSelectedOrgId,
+    selectedOrgId,
+  ]);
   return (
     <div className="fixed inset-0 box-border flex overflow-hidden p-5 bg-gray-50 tablet:p-3">
       <Sidebar />
