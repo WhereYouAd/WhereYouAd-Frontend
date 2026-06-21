8c3c16449a67d4010e2924684e629ae4b37f8e78
JAESEON PARK <ddorri83@naver.com>
2026-06-21 22:45:51 +0900

    feat: 랜딩페이지 GuideTimeline에 하드코딩된 숫자 모아두기

---
+++ src/constants/timeline/layout.ts
@@ -0,0 +1,7 @@
+/*하드코딩 숫자 모아두기*/
+
+export const TIMELINE_COL_WIDTH = 55;
+export const TIMELINE_ROW_HEIGHT = 92;
+export const TIMELINE_ROW_OFFSET = 24;
+export const TIMELINE_AXIS_HEIGHT = 28;
+export const TIMELINE_BAR_HEIGHT = 52;
