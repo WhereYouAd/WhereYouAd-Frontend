58505d3a155074e9b283d549d0521803d003dfdb
서제경 <seojk0315@naver.com>
2026-07-30 10:47:07 +0900

    setting: React 19 createRoot에 Sentry 에러 핸들러 연결 및 App import alias 통일

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,7 @@ import { createRoot } from "react-dom/client";
 import * as Sentry from "@sentry/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./App.tsx";
-
+import App from "@/App";
 import { queryClient } from "@/lib/queryClient";
 
 Sentry.init({
@@ -17,7 +16,10 @@ Sentry.init({
   tracesSampleRate: 0.1,
 });
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!, {
+  onCaughtError: Sentry.reactErrorHandler(),
+  onUncaughtError: Sentry.reactErrorHandler(),
+}).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
